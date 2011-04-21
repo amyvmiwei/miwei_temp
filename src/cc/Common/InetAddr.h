646943382ea2bed96f4fdafb925da8290aec68d2
Sanjit Jhala <sjhalaz@gmail.com>
2011-04-20 18:06:32 -0700

    Added is_ipv4() method to InetAddr to test if a string is an ip addr in the n.n.n.n format.

--- src/cc/Common/InetAddr.h
+++ src/cc/Common/InetAddr.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2007 Doug Judd (Zvents, Inc.)
+ * Copyright (C) 2011 Hypertable, Inc.
  *
  * This file is part of Hypertable.
  *
@@ -114,6 +114,12 @@ namespace Hypertable {
      */
     static bool parse_ipv4(const char *ip, uint16_t port, sockaddr_in &addr,
                            int base = 0);
+
+    /**
+     * Tests whether the input string in n.n.n.n format (base 10)
+     * @param ip - ipv4 string
+     */
+    static bool is_ipv4(const char *ip);
 
     /** Initialize addr from an integer ip address and port */
     static bool initialize(sockaddr_in *addr, uint32_t haddr, uint16_t port);
