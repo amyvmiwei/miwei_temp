7d65ab91404e2e011b85df65c401f7dc8d34e778
Sanjit Jhala <sjhalaz@gmail.com>
2011-08-08 11:34:11 -0700

    [issue 661] Fixed ThriftBroker dies on RangeServer exception
    -The problem here was caused by exceptions being thrown in the TableMutator destructor

--- src/cc/Hypertable/Lib/IntervalScannerAsync.cc
+++ src/cc/Hypertable/Lib/IntervalScannerAsync.cc
@@ -184,10 +184,15 @@ void IntervalScannerAsync::init(const ScanSpec &scan_spec) {
 }
 
 IntervalScannerAsync::~IntervalScannerAsync() {
-  HT_ASSERT(!has_outstanding_requests());
-  // destroy dangling scanner
-  if (m_cur_scanner_id && !m_cur_scanner_finished)
-    m_range_server.destroy_scanner(m_range_info.addr, m_cur_scanner_id, 0);
+  try {
+    HT_ASSERT(!has_outstanding_requests());
+    // destroy dangling scanner
+    if (m_cur_scanner_id && !m_cur_scanner_finished)
+      m_range_server.destroy_scanner(m_range_info.addr, m_cur_scanner_id, 0);
+  }
+  catch(Exception &e) {
+    HT_ERROR_OUT << e << HT_END;
+  }
 }
 
 // caller is responsible for state of m_create_timer
