6b65424603444eb424d58bbd8b04a24b3b1e072d
Doug Judd <doug@hypertable.com>
2011-10-06 10:48:17 -0700

    Upgraded to Thrift 0.7.0

--- src/php/ThriftClient/client_test.php
+++ src/php/ThriftClient/client_test.php
@@ -14,29 +14,29 @@ print_r($client->hql_query($namespace, "select * from thrift_test revs=1 "));
 echo "mutator examples\n";
 $mutator = $client->open_mutator($namespace, "thrift_test", 0, 0);
 
-$key = new Hypertable_ThriftGen_Key(array('row'=> 'php-k1', 'column_family'=> 'col'));
-$cell = new Hypertable_ThriftGen_Cell(array('key' => $key, 'value'=> 'php-v1'));
+$key = new Key(array('row'=> 'php-k1', 'column_family'=> 'col'));
+$cell = new Cell(array('key' => $key, 'value'=> 'php-v1'));
 $client->set_cell($mutator, $cell);
 $client->close_mutator($mutator);
 
 echo "shared mutator examples\n";
-$mutate_spec = new Hypertable_ThriftGen_MutateSpec(array('appname'=>"test-php", 
+$mutate_spec = new MutateSpec(array('appname'=>"test-php", 
                                                          'flush_interval'=>1000, 
                                                          'flags'=> 0));
 
-$key = new Hypertable_ThriftGen_Key(array('row'=> 'php-put-k1', 'column_family'=> 'col'));
-$cell = new Hypertable_ThriftGen_Cell(array('key' => $key, 'value'=> 'php-put-v1'));
+$key = new Key(array('row'=> 'php-put-k1', 'column_family'=> 'col'));
+$cell = new Cell(array('key' => $key, 'value'=> 'php-put-v1'));
 $client->offer_cell($namespace, "thrift_test", $mutate_spec, $cell);
 
-$key = new Hypertable_ThriftGen_Key(array('row'=> 'php-put-k2', 'column_family'=> 'col'));
-$cell = new Hypertable_ThriftGen_Cell(array('key' => $key, 'value'=> 'php-put-v2'));
+$key = new Key(array('row'=> 'php-put-k2', 'column_family'=> 'col'));
+$cell = new Cell(array('key' => $key, 'value'=> 'php-put-v2'));
 $client->refresh_shared_mutator($namespace, "thrift_test", $mutate_spec);
 $client->offer_cell($namespace, "thrift_test", $mutate_spec, $cell);
 sleep(2);
 
 echo "scanner examples\n";
 $scanner = $client->open_scanner($namespace, "thrift_test",
-    new Hypertable_ThriftGen_ScanSpec(array('revs'=> 1)));
+    new ScanSpec(array('revs'=> 1)));
 
 $cells = $client->next_cells($scanner);
 
