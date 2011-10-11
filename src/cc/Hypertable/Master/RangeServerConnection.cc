57e343e06b191ad83d98a32017361758811473d4
Sanjit Jhala <sjhalaz@gmail.com>
2011-10-11 16:17:56 -0700

    Added in_operation bit to master.
    -Bit gets set when the master is restarted and has an INITIALIZE operation in the mml
     or when the first OperationMoveRange is run
    -All rangeservers registered before in_operation=true have their balanced flag set to true
    -This bit is used to distinguish between rangeservers registered when a fresh cluster
     is started and a new rangeserver joining a running cluster. The former should not
     trigger a balance operation but the latter should.

--- src/cc/Hypertable/Master/RangeServerConnection.cc
+++ src/cc/Hypertable/Master/RangeServerConnection.cc
@@ -28,11 +28,15 @@
 
 using namespace Hypertable;
 
-RangeServerConnection::RangeServerConnection(MetaLog::WriterPtr &mml_writer, const String &location,
-                                             const String &hostname, InetAddr public_addr)
+RangeServerConnection::RangeServerConnection(MetaLog::WriterPtr &mml_writer,
+                                             const String &location,
+                                             const String &hostname, InetAddr public_addr,
+                                             bool balanced)
   : MetaLog::Entity(MetaLog::EntityType::RANGE_SERVER_CONNECTION), m_mml_writer(mml_writer),
     m_location(location), m_hostname(hostname), m_state(0), m_removal_time(0),
     m_public_addr(public_addr), m_connected(false) {
+  if (balanced)
+    m_state |= RangeServerConnectionFlags::BALANCED;
   m_comm_addr.set_proxy(m_location);
   m_mml_writer->record_state(this);
 }
