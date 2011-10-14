e3c319286f84b2de7f86c03b2d20405af05d0fff
Doug Judd <doug@hypertable.com>
2011-10-13 17:29:30 -0700

    Added scripts for setting up build env on CentOS, Debian, and OpenSUSE

---
+++ bin/src-utils/build-setup-centos5.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env bash
+
+arch=`uname -m`
+
+if [ $arch == "i386" ] || [ $arch == "i586" ] || [ $arch == "i686" ] ; then
+  ARCH=32
+elif [ $arch == "x86_64" ] ; then
+  ARCH=64
+else
+  echo "Unknown processor architecture: $arch"
+  exit 1
+fi
+
+rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
+
+cd /etc/yum.repos.d
+wget http://dries.eu/pub/dries-el.repo
+
+yum -y install gcc-c++ make cmake python-devel bzip2-devel zlib-devel expat-devel rrdtool rrdtool-devel ruby ruby-devel ruby-ext log4cpp-devel git-core libevent-devel readline-devel ncurses-devel xml-commons-apis freetype-devel libpng-devel libart_lgpl-devel pango pango-devel cairo cairo-devel
+
+# Boost
+cd ~
+wget http://downloads.sourceforge.net/boost/boost_1_44_0.tar.bz2
+tar xjvf boost_1_44_0.tar.bz2
+cd boost_1_44_0
+./bootstrap.sh --with-libraries=filesystem,iostreams,program_options,system,thread,graph,regex
+./bjam install
+cd ~; /bin/rm -rf ~/boost_1_44_0*
+
+# SIGAR
+cd ~
+wget http://www.hypertable.org/pub/hyperic-sigar-1.6.4.zip
+unzip hyperic-sigar-1.6.4.zip
+cp hyperic-sigar-1.6.4/sigar-bin/include/*.h /usr/local/include
+if [ $ARCH -eq 32 ]; then
+  cp hyperic-sigar-1.6.4/sigar-bin/lib/libsigar-x86-linux.so /usr/local/lib
+else
+  cp hyperic-sigar-1.6.4/sigar-bin/lib/libsigar-amd64-linux.so /usr/local/lib
+fi
+/bin/rm -rf ~/hyperic-sigar-1.6.4*
+
+# BerkeleyDB
+cd ~
+wget http://www.hypertable.org/pub/db-5.2.36.tar.gz
+tar -xzvf db-5.2.36.tar.gz
+cd db-5.2.36/build_unix/
+../dist/configure --enable-cxx
+make
+make install
+sh -c "echo '/usr/local/BerkeleyDB.5.2/lib' > /etc/ld.so.conf.d/BerkeleyDB.5.2.conf"
+/bin/rm -rf ~/db-5.2.36*
+
+# Google RE2
+cd ~
+wget http://hypertable.org/pub/re2.tgz
+tar -zxvf re2.tgz
+cd re2
+make
+make install
+/bin/rm -rf ~/re2*
+
+# libunwind
+cd ~
+wget http://download.savannah.gnu.org/releases/libunwind/libunwind-0.99-beta.tar.gz
+tar xzvf libunwind-0.99-beta.tar.gz
+cd libunwind-0.99-beta
+./configure
+make
+make install
+cd ~
+/bin/rm -rf ~/libunwind-0.99-beta*
+
+# Google Perftools
+cd ~
+wget http://google-perftools.googlecode.com/files/google-perftools-1.8.3.tar.gz
+tar xzvf google-perftools-1.8.3.tar.gz
+cd google-perftools-1.8.3
+./configure
+make
+make install
+cd ~
+/bin/rm -rf ~/google-perftools-1.8.3*
+
+# Google Snappy
+cd ~
+wget http://snappy.googlecode.com/files/snappy-1.0.4.tar.gz
+tar xzvf snappy-1.0.4.tar.gz
+cd snappy-1.0.4
+./configure
+make
+make install
+cd ~
+/bin/rm -rf ~/snappy-1.0.4*
+
+# Java JDK
+cd ~
+if [ $ARCH -eq 32 ]; then
+  arch="i586"
+else
+  arch="x64"
+fi
+wget -O jdk-6u25-linux-$arch-rpm.bin http://download.oracle.com/otn-pub/java/jdk/6u25-b06/jdk-6u25-linux-$arch-rpm.bin
+chmod 755 jdk-6u25-linux-$arch-rpm.bin 
+./jdk-6u25-linux-$arch-rpm.bin
+rm -rf ~/jdk-6u25-linux-$arch-rpm*
+
+# Apache Ant
+cd ~
+wget http://apache.deathculture.net//ant/binaries/apache-ant-1.8.2-bin.zip
+cd /usr/share
+unzip ~/apache-ant-1.8.2-bin.zip
+mv ant ant.old
+ln -s apache-ant-1.8.2 ant
+rm -rf /usr/java/default/ant*
+cp apache-ant-1.8.2/lib/*.jar /usr/java/default
+rm -rf ~/apache-ant-1.8.2-bin*
+
+export PATH=$PATH:/usr/share/ant/bin
+
+echo "# CentOS-Testing:" > /etc/yum.repos.d/CentOS-Testing.repo
+echo "[c5-testing]" >> /etc/yum.repos.d/CentOS-Testing.repo
+echo "name=CentOS-5 Testing" >> /etc/yum.repos.d/CentOS-Testing.repo
+echo "baseurl=http://dev.centos.org/centos/\$releasever/testing/\$basearch/" >> /etc/yum.repos.d/CentOS-Testing.repo
+echo "enabled=1" >> /etc/yum.repos.d/CentOS-Testing.repo
+echo "gpgcheck=1" >> /etc/yum.repos.d/CentOS-Testing.repo
+echo "gpgkey=http://dev.centos.org/centos/RPM-GPG-KEY-CentOS-testing" >> /etc/yum.repos.d/CentOS-Testing.repo
+echo "includepkgs=php*" >> /etc/yum.repos.d/CentOS-Testing.repo
+
+yum -y install automake libtool flex bison pkgconfig libevent-devel ruby-devel perl-Bit-Vector php php-cli php-common php-devel
+
+rpm -Uvh http://ftp.belnet.be/packages/dries.ulyssis.org/fedora/fc8/x86_64/RPMS.dries/perl-Class-Accessor-0.31-1.fc8.rf.noarch.rpm
+
+# Thrift
+cd /usr/src
+wget http://www.hypertable.org/pub/thrift-0.7.0.tar.gz
+tar xzvf thrift-0.7.0.tar.gz
+rm -f thrift
+ln -s thrift-0.7.0 thrift
+cd thrift-0.7.0
+chmod 755 ./configure ./lib/php/src/ext/thrift_protocol/build/shtool
+./configure
+make
+make install
+
+yum -y install hadoop-0.20 dstat oprofile tcpdump doxygen rrdtool graphviz gdb emacs-nox valgrind
+
+# Upgrade to ruby 1.8.7
+yum -y remove ruby
+yum -y install gcc gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel
+wget http://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7.tar.gz
+tar xzvf ruby-1.8.7.tar.gz
+cd ruby-1.8.7
+./configure
+make
+make install
+cd ~
+rm -rf ruby-1.8.7*
+hash -r
+
+wget http://rubyforge.org/frs/download.php/69365/rubygems-1.3.6.tgz
+tar xzvf rubygems-1.3.6.tgz
+cd rubygems-1.3.6
+ruby setup.rb
+cd ~
+rm -rf rubygems-1.3.6*
+hash -r
+
+gem install capistrano sinatra rack thin json titleize
+
+/sbin/ldconfig
---
+++ bin/src-utils/build-setup-opensuse.sh
@@ -0,0 +1,148 @@
+#!/usr/bin/env bash
+
+arch=`uname -m`
+
+if [ $arch == "i386" ] || [ $arch == "i586" ] || [ $arch == "i686" ] ; then
+  ARCH=32
+elif [ $arch == "x86_64" ] ; then
+  ARCH=64
+else
+  echo "Unknown processor architecture: $arch"
+  exit 1
+fi
+
+zypper --non-interactive install wget unzip gcc-c++ make cmake python-devel zlib-devel libexpat-devel libbz2-devel rrdtool rrdtool-devel ruby ruby-devel expat
+
+# Boost
+cd ~
+wget http://downloads.sourceforge.net/boost/boost_1_44_0.tar.bz2
+tar xjvf boost_1_44_0.tar.bz2
+cd boost_1_44_0
+./bootstrap.sh --with-libraries=filesystem,iostreams,program_options,system,thread,graph,regex
+./bjam install
+cd ~; /bin/rm -rf ~/boost_1_44_0*
+
+zypper --non-interactive install git-core libevent-devel readline-devel ncurses-devel libpng-devel libart_lgpl-devel
+
+# Cronolog
+cd ~
+wget http://cronolog.org/download/cronolog-1.6.2.tar.gz
+tar xzvf cronolog-1.6.2.tar.gz 
+cd cronolog-1.6.2/
+./configure; make; make install
+cd ~; /bin/rm -rf cronolog-1.6.2*
+
+# SIGAR
+cd ~
+wget http://www.hypertable.org/pub/hyperic-sigar-1.6.4.zip
+unzip hyperic-sigar-1.6.4.zip
+cp hyperic-sigar-1.6.4/sigar-bin/include/*.h /usr/local/include
+if [ $ARCH -eq 32 ]; then
+  cp hyperic-sigar-1.6.4/sigar-bin/lib/libsigar-x86-linux.so /usr/local/lib
+else
+  cp hyperic-sigar-1.6.4/sigar-bin/lib/libsigar-amd64-linux.so /usr/local/lib
+fi
+/bin/rm -rf ~/hyperic-sigar-1.6.4*
+
+# BerkeleyDB
+cd ~
+wget http://www.hypertable.org/pub/db-5.2.36.tar.gz
+tar -xzvf db-5.2.36.tar.gz
+cd db-5.2.36/build_unix/
+../dist/configure --enable-cxx
+make
+make install
+sh -c "echo '/usr/local/BerkeleyDB.5.2/lib' > /etc/ld.so.conf.d/BerkeleyDB.5.2.conf"
+/bin/rm -rf ~/db-5.2.36*
+
+# Google RE2
+cd ~
+wget http://hypertable.org/pub/re2.tgz
+tar -zxvf re2.tgz
+cd re2
+make
+make install
+/bin/rm -rf ~/re2*
+
+# libunwind
+if [ $ARCH -eq 64 ]; then
+  cd ~
+  wget http://download.savannah.gnu.org/releases/libunwind/libunwind-0.99-beta.tar.gz
+  tar xzvf libunwind-0.99-beta.tar.gz
+  cd libunwind-0.99-beta
+  ./configure
+  make
+  make install
+  cd ~
+  /bin/rm -rf ~/libunwind-0.99-beta*
+fi
+
+# Google Perftools
+cd ~
+wget http://google-perftools.googlecode.com/files/google-perftools-1.8.3.tar.gz
+tar xzvf google-perftools-1.8.3.tar.gz
+cd google-perftools-1.8.3
+./configure
+make
+make install
+cd ~
+/bin/rm -rf ~/google-perftools-1.8.3*
+
+# Google Snappy
+cd ~
+wget http://snappy.googlecode.com/files/snappy-1.0.4.tar.gz
+tar xzvf snappy-1.0.4.tar.gz
+cd snappy-1.0.4
+./configure
+make
+make install
+cd ~
+/bin/rm -rf ~/snappy-1.0.4*
+
+# Java JDK
+cd ~
+if [ $ARCH -eq 32 ]; then
+  arch="i586"
+else
+  arch="x64"
+fi
+wget -O jdk-6u25-linux-$arch-rpm.bin http://download.oracle.com/otn-pub/java/jdk/6u25-b06/jdk-6u25-linux-$arch-rpm.bin
+chmod 755 jdk-6u25-linux-$arch-rpm.bin 
+./jdk-6u25-linux-$arch-rpm.bin
+rm -rf ~/jdk-6u25-linux-$arch-rpm*
+
+update-alternatives --install /usr/bin/java java /usr/java/jdk1.6.0_25/bin/java 2000
+update-alternatives --install /usr/bin/javac javac /usr/java/jdk1.6.0_25/bin/javac 2000
+
+# Apache Ant
+cd ~
+wget http://apache.deathculture.net//ant/binaries/apache-ant-1.8.2-bin.zip
+cd /usr/share
+unzip ~/apache-ant-1.8.2-bin.zip
+mv ant ant.old
+ln -s apache-ant-1.8.2 ant
+rm -rf /usr/java/default/ant*
+cp apache-ant-1.8.2/lib/*.jar /usr/java/default
+rm -rf ~/apache-ant-1.8.2-bin*
+
+export PATH=$PATH:/usr/share/ant/bin
+
+zypper --non-interactive install ant automake libtool flex bison pkg-config libevent-devel perl-Bit-Vector php5 php5-devel openssl libopenssl-devel rubygems perl-Class-Accessor
+
+# Thrift
+cd /usr/src
+wget http://www.hypertable.org/pub/thrift-0.7.0.tar.gz
+tar xzvf thrift-0.7.0.tar.gz
+rm -f thrift
+ln -s thrift-0.7.0 thrift
+cd thrift-0.7.0
+chmod 755 ./configure ./lib/php/src/ext/thrift_protocol/build/shtool
+./configure
+make
+make install
+
+zypper --non-interactive install dstat doxygen rrdtool graphviz gdb emacs-nox 
+
+gem install capistrano sinatra rack thin json titleize
+
+/sbin/ldconfig
