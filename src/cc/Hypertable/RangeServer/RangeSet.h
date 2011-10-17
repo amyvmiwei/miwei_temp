055e9b7ae74d5348088de3e7ff163c3e51413dc4
Sanjit Jhala <sjhalaz@gmail.com>
2011-10-17 15:59:27 -0700

    Fixed minor build warning.

--- src/cc/Hypertable/RangeServer/RangeSet.h
+++ src/cc/Hypertable/RangeServer/RangeSet.h
@@ -60,8 +60,8 @@ namespace Hypertable {
      * @param end_row end row key for range
      * @return true if range found and end row changed, false otherwise
      */
-    virtual bool change_start_row(const String &old_start_row, const String &new_end_row,
-                                  const String &new_end_row) = 0;
+    virtual bool change_start_row(const String &old_start_row, const String &new_start_row,
+                                  const String &end_row) = 0;
 
   };
 
