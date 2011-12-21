0c04dd7aa69923e5d3f1339c36e146675267a72a
Doug Judd <doug@hypertable.com>
2011-12-21 02:45:28 -0800

    Fixed thrift-reconnect-hyperspace test

--- tests/integration/thrift-reconnect-hyperspace/run.sh
+++ tests/integration/thrift-reconnect-hyperspace/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+HT_TEST_DFS=${HT_TEST_DFS:-local}
+
 set -v
 
 TEST_BIN=./client_test
@@ -7,11 +9,11 @@ HT_HOME=${INSTALL_DIR:-"$HOME/hypertable/current"}
 
 $HT_HOME/bin/start-test-servers.sh --clean
 
-sleep 7;
+sleep 5;
 $HT_HOME/bin/stop-servers.sh --no-thriftbroker 
-sleep 7;
-$HT_HOME/bin/start-test-servers.sh --no-thriftbroker
-sleep 7;
+sleep 3;
+$HT_HOME/bin/start-all-servers.sh --no-thriftbroker $HT_TEST_DFS
+sleep 12;
 
 cd ${THRIFT_CPP_TEST_DIR};
 ${TEST_BIN}
