d1d68d4dfad5bb1e4a16c01cf500d288c250de28
Doug Judd <doug@hypertable.com>
2012-01-29 13:12:16 -0800

    More performance test updates; Changed 0.9.5.5 release date

--- examples/java/org/hypertable/examples/PerformanceTest/bin/run-test-read-random.sh
+++ examples/java/org/hypertable/examples/PerformanceTest/bin/run-test-read-random.sh
@@ -7,7 +7,7 @@ export BINDIR=$(cd `dirname "$0"` && pwd)
 
 usage() {
   echo ""
-  echo "usage: run-test-read-random.sh <system> <key-size> <value-size>"
+  echo "usage: run-test-read-random.sh [--zipfian] <system> <key-size> <value-size>"
   echo ""
   echo "This script is used to run a performance benchmark.  The <system>"
   echo "argument indicates which system to run the test against and"
@@ -15,7 +15,16 @@ usage() {
   echo ""
 }
 
-if [ "$#" -ne 3 ]; then
+ZIPF_ARGS=
+if [ "$#" -eq 4 ] ; then
+  if [ "$1" == "--zipfian" ]; then
+    shift
+    ZIPF_ARGS="--zipf --cmf-file=$CMF"
+  else
+    usage
+    exit 1
+  fi
+elif [ "$#" -ne 3 ] ; then
   usage
   exit 1
 fi
@@ -39,31 +48,14 @@ else
     exit 1
 fi
 
-
 let CELLSIZE=KEY_SIZE+VALUE_SIZE
 let KEYMAX=DATA_SIZE/CELLSIZE
 let DISTRANGE=100000000
 let KEYCOUNT=100000000
 
-#
-# This file should be the same on all test Client machines.  I can be
-# generated with the following command:
-#
-# /opt/hypertable/doug/current/bin/jrun org.hypertable.Common.DiscreteRandomGeneratorZipf --generate-cmf-file /data/1/test/cmf.dat 0 100000000
-#
-CMF=/data/1/test/cmf.dat
 
 ${STOP_SYSTEM}
-CONFIG=$PWD/hypertable-read-random-uniform.cfg
 ${START_SYSTEM}
 sleep 300
 
-cap -S test_driver=$SYSTEM -S client_multiplier=128 -S test_args="--test-name=$TEST_NAME --output-dir=$REPORT_DIR --random --key-max=$KEYMAX --submit-exactly=$KEYCOUNT read $KEY_SIZE $VALUE_SIZE $DATA_SIZE" run_test 
-
-${STOP_SYSTEM}
-CONFIG=$PWD/hypertable-read-random-zipfian.cfg
-${START_SYSTEM}
-sleep 300
-
-cap -S test_driver=$SYSTEM -S client_multiplier=128 -S test_args="--test-name=$TEST_NAME --output-dir=$REPORT_DIR --random --zipf --cmf-file=$CMF --key-max=$KEYMAX --submit-exactly=$KEYCOUNT read $KEY_SIZE $VALUE_SIZE $DATA_SIZE" run_test 
-
+cap -S test_driver=$SYSTEM -S client_multiplier=128 -S test_args="--test-name=$TEST_NAME --output-dir=$REPORT_DIR --random $ZIPF_ARGS --key-max=$KEYMAX --submit-exactly=$KEYCOUNT read $KEY_SIZE $VALUE_SIZE $DATA_SIZE" run_test 
