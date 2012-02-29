a338e2067a51878051d0306ae92ff66634f9aa02
Christoph Rupp <chris@crupp.de>
2012-02-29 10:56:37 +0100

    issue 779: Fixed upgrade-ok.sh for 1.0.0.0

--- bin/upgrade-ok.sh
+++ bin/upgrade-ok.sh
@@ -106,10 +106,8 @@ if [ $TO_MINOR -le 1000000 ] && [ $TO_MINOR -ge 904000 ] ; then
     echo "Incompatible upgrade a: $FROM -> $TO"
     exit 1
 elif [ $TO_MAJOR -ge 1000000 ] ; then
-    if [ $FROM_MAJOR -ge 1000000 ] ; then
-        if [ $FROM_MINOR -eq $TO_MINOR ] ; then
-            exit 0
-        fi
+    if [ $FROM_MAJOR -le $TO_MAJOR ] ; then
+        exit 0
     fi
     echo "Incompatible upgrade b: $FROM -> $TO"
     exit 1
---
+++ tests/integration/defects/issue779/run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+HT_HOME=${INSTALL_DIR:-"/opt/hypertable/current"}
+SCRIPT_DIR=`dirname $0`
+
+echo "======================="
+echo "Defect #779"
+echo "======================="
+
+check ()
+{
+    from=$1
+    to=$2
+    expected=$3
+    echo "$from -> $to"
+    $HT_HOME/bin/upgrade-ok.sh $from $to
+    if [ "$?" -ne "$expected" ]
+    then
+        echo "return code mismatch"
+        exit 1
+    fi
+}
+
+# positive tests
+check 1.1.1.1 1.1.1.2 0
+check 1.0.0.0 1.0.1.0 0
+check 0.9.6.0 1.0.0.0 0
+check 0.9.5.0 1.0.0.0 0
+check 0.9.5.0 0.9.6.0 0
+check 0.9.4.0 1.0.0.0 0
+
+check 0.9.6.0 0.9.5.2 0
+
+# negative tests
+check 1.1.1.2 1.1.1.1 1
+check 1.0.1.1 1.0.1.0 1
+check 1.0.0.0 0.9.6.0 1
+check 1.0.0.0 0.9.5.5 1
+check 1.0.0.0 0.9.4.1 1
+
+echo "SUCCESS"
