2144afbfecb46f22a2670b6b1bf0b6178dc8787f
Doug Judd <doug@hypertable.com>
2012-07-28 15:45:06 -0700

    Added process ID to valgrind filename

--- bin/start-rangeserver.sh
+++ bin/start-rangeserver.sh
@@ -36,7 +36,7 @@ usage() {
 while [ "$1" != "${1##[-+]}" ]; do
   case $1 in
     --valgrind)
-      VALGRIND="valgrind -v --log-file=vg --leak-check=full --num-callers=20 "
+      VALGRIND="valgrind -v --log-file=vg.rs.%p --leak-check=full --num-callers=20 "
       shift
       ;;
     --heapcheck)
