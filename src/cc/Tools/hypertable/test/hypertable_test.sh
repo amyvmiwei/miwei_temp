44987e55fab1e70c8c6c352b7efacb0b8ca7ad03
Christoph Rupp <chris@crupp.de>
2013-01-08 14:58:32 +0100

    Hypertable Shell test no longer fails if it is restarted

---
+++ src/cc/Tools/hypertable/test/hypertable_test.sh
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env bash
+
+HT_HOME=${INSTALL_DIR:-"/opt/hypertable/current"}
+SCRIPT_DIR=`dirname $0`
+
+echo "======================================"
+echo "Hypertable shell test"
+echo "======================================"
+$HT_HOME/bin/start-test-servers.sh --clear
+
+./hypertable_test
+
