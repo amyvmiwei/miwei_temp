7156c8e3a1b6174880670518b47b239c7ec70ea6
Doug Judd <doug@hypertable.com>
2013-02-01 11:37:02 -0800

    issue976: fixed rows+cols bloom filter access with qualifier regex

--- src/cc/Hypertable/RangeServer/tests/CellStore64_test.cc
+++ src/cc/Hypertable/RangeServer/tests/CellStore64_test.cc
@@ -39,7 +39,7 @@
 #include "Hypertable/Lib/SerializedKey.h"
 
 #include "../CellStoreFactory.h"
-#include "../CellStoreV5.h"
+#include "../CellStoreV6.h"
 #include "../Global.h"
 
 #include <cstdlib>
@@ -117,7 +117,7 @@ int main(int argc, char **argv) {
     Config::properties->set("Hypertable.RangeServer.CellStore.DefaultCompressor", String("none"));
     Config::properties->set("Hypertable.RangeServer.CellStore.DefaultBlockSize", 4*1024*1024);
 
-    cs = new CellStoreV5(Global::dfs.get());
+    cs = new CellStoreV6(Global::dfs.get());
     HT_TRY("creating cellstore", cs->create(csname.c_str(), 4096, Config::properties, &table_id));
 
     // setup value
--- src/cc/Hypertable/RangeServer/tests/CellStoreScanner_test.cc
+++ src/cc/Hypertable/RangeServer/tests/CellStoreScanner_test.cc
@@ -40,7 +40,7 @@
 #include "Hypertable/Lib/SerializedKey.h"
 
 #include "../CellStoreFactory.h"
-#include "../CellStoreV5.h"
+#include "../CellStoreV6.h"
 #include "../Global.h"
 
 #include <cstdlib>
@@ -622,6 +622,7 @@ int main(int argc, char **argv) {
 
     String csname = testdir + "/cs0";
     PropertiesPtr cs_props = new Properties();
+    Schema::parse_bloom_filter("rows+cols", cs_props);
 
     SchemaPtr schema = Schema::new_instance(schema_str, strlen(schema_str));
 
@@ -630,7 +631,7 @@ int main(int argc, char **argv) {
       exit(1);
     }
 
-    cs = new CellStoreV5(Global::dfs.get(), schema.get());
+    cs = new CellStoreV6(Global::dfs.get(), schema.get());
     HT_TRY("creating cellstore", cs->create(csname.c_str(), 0, cs_props, &table_id));
     cs->set_replaced_files(replaced_files_write);
 
@@ -762,6 +763,19 @@ int main(int argc, char **argv) {
         HT_ASSERT(count == 1);
       }
     }
+
+    /**
+     * issue 976
+     */
+    out << "[issue976]\n";
+    ssbuilder.clear();
+    ssbuilder.add_row_interval("http://www.Texas.com/", true,
+                               "http://www.Texas.com/", true);
+    ssbuilder.add_column("tag:^s");
+    scan_ctx = new ScanContext(TIMESTAMP_MAX, &(ssbuilder.get()), &range,
+                                   schema);
+    out << "may_contain(ROW=\"http://www.Texas.com/\", COL=\"tag:^s\") == ";
+    out << ((cs->may_contain(scan_ctx)) ? "true" : "false") << "\n";
 
     /**
      * Row operations
@@ -1420,9 +1434,10 @@ int main(int argc, char **argv) {
     display_scan(scanner, out);
 
     csname = testdir + "/cs1";
+    cs_props = new Properties();
     cs_props->set("blocksize", (uint32_t)10000);
     cs_props->set("compressor", String("none"));
-    cs = new CellStoreV5(Global::dfs.get(), schema.get());
+    cs = new CellStoreV6(Global::dfs.get(), schema.get());
     HT_TRY("creating cellstore", cs->create(csname.c_str(), 0, cs_props, &table_id));
     // should not coalesce and be in a separate block from trailer
     replaced_files_write.push_back("1/hypertable/tables/0/1/default/qyoNKN5rd__dbHKv/cs0");
@@ -1535,7 +1550,7 @@ int main(int argc, char **argv) {
       exit(1);
     }
 
-    cs = new CellStoreV5(Global::dfs.get(), schema.get());
+    cs = new CellStoreV6(Global::dfs.get(), schema.get());
     HT_TRY("creating cellstore", cs->create(csname.c_str(), 0, cs_props, &table_id));
     // should coalesce and be in 2 blocks, with the 2nd block also containing the trailer
     replaced_files_write.push_back("7/hypertable/tables/0/1/default/qyoNKN5rd__dbHKv/cs0");
@@ -1568,8 +1583,8 @@ int main(int argc, char **argv) {
     }
 
     cs->finalize(&table_id);
-    out << "[replaced-files-2]\n";
     cs = CellStoreFactory::open(csname, "", "row000200/");
+    out << "[replaced-files-2]\n";
     check_replaced_files(cs, replaced_files_write, out);
 
     int64_t expiration_time = boost::any_cast<int64_t>(cs->get_trailer()->get("expiration_time"));
