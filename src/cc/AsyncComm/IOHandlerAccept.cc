f06b3661efc96a0cf3914772af2dcf735c40c306
Doug Judd <doug@hypertable.com>
2013-05-31 17:20:50 -0700

    Don't decomission handler after propagate_proxy_map() failure

--- src/cc/AsyncComm/IOHandlerAccept.cc
+++ src/cc/AsyncComm/IOHandlerAccept.cc
@@ -147,8 +147,7 @@ bool IOHandlerAccept::handle_incoming_connection() {
                 Error::get_text(error));
       ReactorRunner::handler_map->decrement_reference_count(handler);
       ReactorRunner::handler_map->decomission_handler(handler);
-      ReactorRunner::handler_map->decomission_handler(this);
-      return true;
+      return false;
     }
 
     if (ReactorFactory::proxy_master) {
@@ -157,8 +156,7 @@ bool IOHandlerAccept::handle_incoming_connection() {
         HT_ERRORF("Problem sending proxy map to %s - %s",
                   m_addr.format().c_str(), Error::get_text(error));
         ReactorRunner::handler_map->decrement_reference_count(handler);
-        ReactorRunner::handler_map->decomission_handler(handler);
-        return true;
+        return false;
       }
     }
 
