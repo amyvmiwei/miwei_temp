10b351ca5ca2292b3ba6734edb987c7357168288
Doug Judd <doug@hypertable.com>
2013-08-27 01:03:40 -0700

    issue1125: Fixed AccessGroup-garbage-tracker test

--- src/cc/Hypertable/RangeServer/tests/AccessGroupGarbageTracker_test.cc
+++ src/cc/Hypertable/RangeServer/tests/AccessGroupGarbageTracker_test.cc
@@ -144,7 +144,7 @@ int main(int argc, char **argv) {
     tracker.clear();
     tracker.accumulate_data(amount);
     tracker.set_garbage_stats(1000000LL, 700000LL);
-    HT_ASSERT(tracker.current_target() == 71582788);
+    HT_ASSERT(tracker.current_target() == 71582788 || tracker.current_target() == 71582787);
     HT_ASSERT(tracker.need_collection());
 
     tracker.clear();
