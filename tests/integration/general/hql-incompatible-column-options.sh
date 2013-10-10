9ab074b7df6aa948a3c5aa6ec6c96ea4b281a058
Doug Judd <doug@hypertable.com>
2013-10-09 21:44:58 -0700

    issue 1155: Throw exception during CREATE TABLE on bad column option combinations

---
+++ tests/integration/general/hql-incompatible-column-options.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+HT_HOME=${INSTALL_DIR:-"$HOME/hypertable/current"}
+SCRIPT_DIR=`dirname $0`
+
+$HT_HOME/bin/start-test-servers.sh --clear --no-thriftbroker
+
+echo "create table foo (a COUNTER TIME_ORDER DESC);" | $HT_HOME/bin/ht shell --batch
+if [ $? -eq 0 ]; then
+    $HT_HOME/bin/stop-servers.sh
+    echo "Column options COUNTER and TIME_ORDER DESC should not have been allowed!"
+    exit 1
+fi
+
+echo "create table foo (a COUNTER MAX_VERSIONS 1);" | $HT_HOME/bin/ht shell --batch
+if [ $? -eq 0 ]; then
+    $HT_HOME/bin/stop-servers.sh
+    echo "Column options COUNTER and MAX_VERSIONS 1 should not have been allowed!"
+    exit 1
+fi
+
+$HT_HOME/bin/stop-servers.sh
+exit 0
+
