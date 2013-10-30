838c9046e7b7492e94005442704cc6ab525c94fe
Doug Judd <doug@hypertable.com>
2013-10-30 15:11:22 -0700

    issue 1138: Fixed DELETE bug on specific cell version w/o qualifier

---
+++ tests/integration/general/hql-delete-test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+HT_HOME=${INSTALL_DIR:-"$HOME/hypertable/current"}
+SCRIPT_DIR=`dirname $0`
+
+$HT_HOME/bin/start-test-servers.sh --clear --no-thriftbroker
+
+echo "CREATE NAMESPACE MATCH_VERSION; quit;" | $HT_HOME/bin/ht shell --batch
+
+echo "USE MATCH_VERSION; CREATE TABLE VERSION('M' MAX_VERSIONS 10); quit;" | $HT_HOME/bin/ht shell --batch
+	
+echo "USE MATCH_VERSION; INSERT INTO VERSION VALUES('rowname', 'M', 'VALUE');  quit;" | $HT_HOME/bin/ht shell --batch
+sleep 1
+echo "USE MATCH_VERSION; INSERT INTO VERSION VALUES('rowname', 'M', 'VALUE');  quit;" | $HT_HOME/bin/ht shell --batch
+sleep 1
+echo "USE MATCH_VERSION; INSERT INTO VERSION VALUES('rowname', 'M', 'VALUE');  quit;" | $HT_HOME/bin/ht shell --batch
+
+echo "[first dump]"
+echo "USE MATCH_VERSION; SELECT M FROM VERSION DISPLAY_TIMESTAMPS;" | $HT_HOME/bin/ht shell --batch | tee "dump1.tsv"
+
+NEWEST_TS=`head -1 dump1.tsv | cut -f1`
+
+echo "USE MATCH_VERSION; DELETE M FROM VERSION WHERE ROW='rowname' VERSION '$NEWEST_TS';" | $HT_HOME/bin/ht shell --batch
+
+echo "[second dump]"
+echo "USE MATCH_VERSION; SELECT M FROM VERSION DISPLAY_TIMESTAMPS;" | $HT_HOME/bin/ht shell --batch | tee "dump2.tsv"
+
+COUNT=`wc -l dump2.tsv | cut -f1 -d' '`
+
+if [ $COUNT -ne 2 ]; then
+  echo "dump2.tsv does not contain exactly two lines!"
+  exit 1
+fi
+
+exit 0
