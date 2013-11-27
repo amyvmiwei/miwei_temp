57e89402291281ad5f64f3b10ccd71457ce89cbb
Doug Judd <doug@hypertable.com>
2013-11-26 16:23:02 -0800

    Use random start time for periodic flushing of shared mutators

--- src/cc/Hypertable/Lib/TableMutatorIntervalHandler.cc
+++ src/cc/Hypertable/Lib/TableMutatorIntervalHandler.cc
@@ -1,5 +1,5 @@
-/** -*- C++ -*-
- * Copyright (C) 2007-2012 Hypertable, Inc.
+/* -*- c++ -*-
+ * Copyright (C) 2007-2013 Hypertable, Inc.
  *
  * This file is part of Hypertable.
  *
@@ -17,9 +17,18 @@
  * along with Hypertable. If not, see <http://www.gnu.org/licenses/>
  */
 
+/** @file
+ * Definitions for TableMutatorIntervalHandler.
+ * This file contains definitions for TableMutatorIntervalHandler, a class that
+ * is used as the timer handler for periodically flushing a shared mutator.
+ */
+
 #include "Common/Compat.h"
+#include "Common/md5.h"
 
 #include "AsyncComm/Comm.h"
+
+#include "HyperAppHelper/Unique.h"
 
 #include "TableMutatorShared.h"
 #include "TableMutatorIntervalHandler.h"
@@ -27,12 +36,29 @@
 
 using namespace Hypertable;
 
+TableMutatorIntervalHandler::TableMutatorIntervalHandler(Comm *comm,
+					 ApplicationQueueInterface *app_queue,
+					 TableMutatorShared *shared_mutator)
+  : active(true), complete(false), comm(comm), app_queue(app_queue), shared_mutator(shared_mutator) {
+  char unique_hash[33];
+  uint32_t first_interval;
+  String str = HyperAppHelper::generate_guid();
+
+  md5_string(str.c_str(), unique_hash);
+
+  memcpy(&first_interval, unique_hash, 4);
+
+  first_interval %= shared_mutator->flush_interval();
+
+  HT_ASSERT(comm->set_timer(first_interval, this) == Error::OK);
+}
+
 
 void TableMutatorIntervalHandler::handle(EventPtr &event) {
 
   if (active) {
     app_queue->add(new TableMutatorFlushHandler(this, event));
-    self_register();
+    HT_ASSERT(comm->set_timer(shared_mutator->flush_interval(), this) == Error::OK);
   }
   else {
     ScopedLock lock(mutex);
@@ -42,13 +68,9 @@ void TableMutatorIntervalHandler::handle(EventPtr &event) {
 }
 
 
-void TableMutatorIntervalHandler::self_register() {
-  HT_ASSERT(comm->set_timer(mutator->flush_interval(), this) == Error::OK);
-}
-
 void TableMutatorIntervalHandler::flush() {
   ScopedLock lock(mutex);
 
   if (active)
-    mutator->interval_flush();
+    shared_mutator->interval_flush();
 }
