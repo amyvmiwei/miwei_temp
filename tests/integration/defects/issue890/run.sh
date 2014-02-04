2e1cf88b13b56c19e556097ad57c9450f2934e4e
Doug Judd <doug@hypertable.com>
2014-02-04 09:54:23 -0800

    Modified issue890 test to compile java file into build directory

--- tests/integration/defects/issue890/run.sh
+++ tests/integration/defects/issue890/run.sh
@@ -10,9 +10,11 @@ echo "======================="
 # make sure hypertable jar files are copied into lib/java
 $HT_HOME/bin/set-hadoop-distro.sh cdh3
 
+cp $SCRIPT_DIR/TestSerializers.java .
+
 echo "compiling"
 JARS=`perl $SCRIPT_DIR/jars.pl $HT_HOME`
-javac -classpath "$JARS:." $SCRIPT_DIR/TestSerializers.java
+javac -classpath "$JARS:." TestSerializers.java
 
 echo "running"
-java -ea -classpath $HT_HOME/lib/java/*:$SCRIPT_DIR:. TestSerializers
+java -ea -classpath $HT_HOME/lib/java/*:. TestSerializers
