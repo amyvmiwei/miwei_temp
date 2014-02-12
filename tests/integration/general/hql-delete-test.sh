2429ed7cff7ffeb62b9e286c5a371cd2f3a66229
Doug Judd <doug@hypertable.com>
2014-02-12 09:23:15 -0800

    Fixed HQL-delete test

--- tests/integration/general/hql-delete-test.sh
+++ tests/integration/general/hql-delete-test.sh
@@ -10,17 +10,17 @@ echo "CREATE NAMESPACE MATCH_VERSION; quit;" | $HT_HOME/bin/ht shell --batch
 echo "USE MATCH_VERSION; CREATE TABLE VERSION('M' MAX_VERSIONS 10); quit;" | $HT_HOME/bin/ht shell --batch
 	
 echo "USE MATCH_VERSION; INSERT INTO VERSION VALUES('rowname', 'M', 'VALUE');  quit;" | $HT_HOME/bin/ht shell --batch
-sleep 1
 echo "USE MATCH_VERSION; INSERT INTO VERSION VALUES('rowname', 'M', 'VALUE');  quit;" | $HT_HOME/bin/ht shell --batch
-sleep 1
-echo "USE MATCH_VERSION; INSERT INTO VERSION VALUES('rowname', 'M', 'VALUE');  quit;" | $HT_HOME/bin/ht shell --batch
+sleep 2
+LAST_TS=`date +"%F %T"`
+echo "USE MATCH_VERSION; INSERT INTO VERSION VALUES('$LAST_TS', 'rowname', 'M', 'VALUE');  quit;" | $HT_HOME/bin/ht shell --batch
 
 echo "[first dump]"
 echo "USE MATCH_VERSION; SELECT M FROM VERSION DISPLAY_TIMESTAMPS;" | $HT_HOME/bin/ht shell --batch | tee "dump1.tsv"
 
-NEWEST_TS=`head -1 dump1.tsv | cut -f1`
-
-echo "USE MATCH_VERSION; DELETE M FROM VERSION WHERE ROW='rowname' VERSION '$NEWEST_TS';" | $HT_HOME/bin/ht shell --batch
+# Delete last insert
+echo "USE MATCH_VERSION; DELETE M FROM VERSION WHERE ROW='rowname' VERSION '$LAST_TS';"
+echo "USE MATCH_VERSION; DELETE M FROM VERSION WHERE ROW='rowname' VERSION '$LAST_TS';" | $HT_HOME/bin/ht shell --batch
 
 echo "[second dump]"
 echo "USE MATCH_VERSION; SELECT M FROM VERSION DISPLAY_TIMESTAMPS;" | $HT_HOME/bin/ht shell --batch | tee "dump2.tsv"
