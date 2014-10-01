99a67691b0273775cc1d8778327d866223ec3e3f
Doug Judd <doug@hypertable.com>
2014-10-01 14:36:36 -0700

    Finished interactive shell

--- src/cc/Tools/cluster/ClusterCommandInterpreter.cc
+++ src/cc/Tools/cluster/ClusterCommandInterpreter.cc
@@ -46,28 +46,32 @@ using namespace std;
 
 namespace {
 
-  const string escape_string(const string &str) {
-    string escaped_str;
-    const char *ptr;
-    for (ptr = str.c_str(); *ptr; ptr++) {
-      if (isspace(*ptr))
+  bool split_command_line(const string &line, vector<string> &argv) {
+    const char *base = line.c_str();
+    while (*base) {
+      while (*base && isspace(*base))
+        base++;
+      if (*base == 0)
         break;
-    }
-    if (*ptr == 0)
-      return str;
-    escaped_str.reserve(str.length()*2);
-    escaped_str.append(1, '"');
-    for (ptr = str.c_str(); *ptr; ptr++) {
-      if (*ptr == '"') {
-        if (ptr == str.c_str() || *(ptr-1) != '\\')
-          escaped_str.append(1, '\\');
-        escaped_str.append(1, '"');
+      const char *ptr = base;
+      if (*ptr == '"' || *ptr == '\'') {
+        const char *end;
+        if (!TokenizerTools::find_end_char(ptr, &end)) {
+          cout << "Invalid command line - missing terminating quote" << endl;
+          return false;
+        }
+        ptr++;
+        argv.push_back(string(ptr, (end-ptr)-1));
+        base = end+1;
       }
-      else
-        escaped_str.append(1, *ptr);
+      else {
+        while (*ptr && !isspace(*ptr))
+          ptr++;
+        argv.push_back(string(base, ptr-base));
+        base = ptr;
+      }
     }
-    escaped_str.append(1, '"');
-    return escaped_str;
+    return true;
   }
 
   bool extract_command_line_argument(const char *base, const char **end, string &arg) {
@@ -105,14 +109,27 @@ ClusterCommandInterpreter::ClusterCommandInterpreter(const string &script)
 
 
 void ClusterCommandInterpreter::execute_line(const String &line) {
-  char *argv[5];
+  char **argv;
   const char *end;
+  string program;
   string target;
   string command;
   string trimmed_line(line);
   boost::trim(trimmed_line);
 
-  if (!strncmp(trimmed_line.c_str(), "on ", 3)) {
+  if (trimmed_line[0] == '!') {
+    vector<string> args;
+    if (!split_command_line(trimmed_line.substr(1), args) || args.empty())
+      return;
+    program = m_command_script;
+    argv = new char *[args.size()+2];
+    size_t i=0;
+    argv[i++] = (char *)program.c_str();
+    for (auto & arg : args)
+      argv[i++] = (char *)arg.c_str();
+    argv[i] = 0;
+  }
+  else if (!strncmp(trimmed_line.c_str(), "on ", 3)) {
 
     if (!extract_command_line_argument(trimmed_line.c_str() + 3, &end, target))
       return;
@@ -125,31 +142,15 @@ void ClusterCommandInterpreter::execute_line(const String &line) {
 
     boost::trim_if(target, boost::is_any_of("'\""));
 
-    string ssh_binary;
-    ssh_binary.append(System::install_dir.c_str());
-    ssh_binary.append("/bin/ht_ssh");
+    program.append(System::install_dir.c_str());
+    program.append("/bin/ht_ssh");
 
-    argv[0] = (char *)ssh_binary.c_str();
+    argv = new char *[4];
+    argv[0] = (char *)program.c_str();
     argv[1] = (char *)target.c_str();
     argv[2] = (char *)command.c_str();
     argv[3] = nullptr;
 
-    pid_t pid = vfork();
-
-    if (pid == 0) {
-      if (execv(ssh_binary.c_str(), argv) < 0) {
-        cout << "execv() failed - " << strerror(errno) << endl;
-        _exit(1);
-      }
-    }
-    else if (pid < 0) {
-      cout << "vfork() failed - " << strerror(errno) << endl;
-      _exit(1);
-    }
-    else {
-      int status;
-      waitpid(pid, &status, 0);
-    }
   }
   else {
 
@@ -170,29 +171,32 @@ void ClusterCommandInterpreter::execute_line(const String &line) {
 
     boost::trim_if(target, boost::is_any_of("'\""));
 
-    argv[0] = (char *)m_command_script.c_str();
+    program = m_command_script;
+    argv = new char *[5];
+    argv[0] = (char *)program.c_str();
     argv[1] = (char *)"with";
     argv[2] = (char *)target.c_str();
     argv[3] = (char *)command.c_str();
     argv[4] = nullptr;
+  }
 
-    pid_t pid = vfork();
+  pid_t pid = vfork();
 
-    if (pid == 0) {
-      if (execv(m_command_script.c_str(), argv) < 0) {
-        cout << "execv() failed - " << strerror(errno) << endl;
-        _exit(1);
-      }
-    }
-    else if (pid < 0) {
-      cout << "vfork() failed - " << strerror(errno) << endl;
+  if (pid == 0) {
+    if (execv(program.c_str(), argv) < 0) {
+      cout << "execv(" << program << ") failed - " << strerror(errno) << endl;
       _exit(1);
     }
-    else {
-      int status;
-      waitpid(pid, &status, 0);
-    }
-    
   }
+  else if (pid < 0) {
+    cout << "vfork() failed - " << strerror(errno) << endl;
+    _exit(1);
+  }
+  else {
+    int status;
+    waitpid(pid, &status, 0);
+    delete [] argv;
+  }
+
 }
 
